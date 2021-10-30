0d72baa72d2458fa60828f6665cefcb90d7d0d24
tengfone <phangtf@gmail.com>
2021-10-30 22:48:29 +0800

    Added remove

--- src/Item.js
+++ src/Item.js
@@ -1,42 +1,43 @@
-import './index.css';
-import { useState, useEffect } from 'react';
+import "./index.css";
+import { useState, useEffect } from "react";
 import { FaMinus } from "react-icons/fa";
 
+const Item = ({ item , xkey, todel }) => {
+  const [itemName, setItemName] = useState("");
+  const [itemQty, setItemQty] = useState("");
 
-const Item = ({item}) => {
-    const [itemName, setItemName] = useState('')
-    const [itemQty, setItemQty] = useState('')
-   return  (
-            <div className="row">
-                <div class="col-md-6">
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    required
-                    value={item.name}
-                    onChange={(e) => setItemName(e.target.value)}
-                    />
-                </div>
-                    <div class="col-md-1">
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    required
-                    value={item.quantity}
-                    onChange={(e) => setItemQty(e.target.value)}
-                    />
-                </div>
-                <div class="col-md-2">
-                    <button
-                    type="button"
-                    className="btn btn-danger"
-                    // onClick={(e) => {deleteRecord(e, order._id)}}
-                    > <FaMinus  />
-                </button>
-                </div>
-            </div>
-            
-   )
-}
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <input
+          type="text"
+          className="form-control"
+          required
+          value={item.name}
+          onChange={(e) => setItemName(e.target.value)}
+        />
+      </div>
+      <div className="col-md-1">
+        <input
+          type="text"
+          className="form-control"
+          required
+          value={item.quantity}
+          onChange={(e) => setItemQty(e.target.value)}
+        />
+      </div>
+      <div className="col-md-2">
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={(e) => {todel(xkey)}}
+        >
+          {" "}
+          <FaMinus />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
--- src/Record.js
+++ src/Record.js
@@ -1,94 +1,125 @@
-import './index.css';
-import { useState, useEffect } from 'react';
+import "./index.css";
+import { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import Item from './Item'
-const axios = require('axios');
+import Item from "./Item";
+const axios = require("axios");
 
 const Record = () => {
+  const { id } = useParams();
+  const [order, setOrder] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [email, setEmail] = useState("");
+  const [hall, setHall] = useState("");
+  const [cart, setCart] = useState([]);
 
-    const {id} = useParams()
-    const [order, setOrder] = useState(null)
-    const [isLoading, setIsLoading] = useState(true);
-    const [name, setName] = useState('')
-    const [contact, setContact] = useState('')
-    const [email, setEmail] = useState('')
-    const [hall, setHall] = useState('')
+  useEffect(() => {
+    axios
+      .get("https://swiftys-server.glitch.me/api/orders/order/" + id)
+      .then((res) => {
+        setOrder(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get("https://swiftys-server.glitch.me/api/orders/order/" + id)
-            .then(res => {
-                setOrder(res.data)
-                setIsLoading(false)
-            }).catch(err => {
-                console.log(err)
-            })
-    }, [])
+  const todel = (i) => {
+    const remainingCart = order.cart.filter((el, idx) => {
+      return i !== idx;
+    });
+    setOrder({ ...order, cart: remainingCart });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // const blog = {title, body, author}
-        // console.log(blog)
-        // setIsLoading(true)
-        // fetch('http://localhost:8000/blogs', {
-        //     method: 'POST',
-        //     headers: {"Content-Type": "application/json"},
-        //     body: JSON.stringify(blog)
-        // }).then(() => {
-        //     console.log("new blog added")
-        //     setIsLoading(false);
-        //     history.push('/');
-        // })
-    }
-   
-    return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const blog = {title, body, author}
+    // console.log(blog)
+    // setIsLoading(true)
+    // fetch('http://localhost:8000/blogs', {
+    //     method: 'POST',
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify(blog)
+    // }).then(() => {
+    //     console.log("new blog added")
+    //     setIsLoading(false);
+    //     history.push('/');
+    // })
+  };
+
+  return (
     <div className="edit">
-        {isLoading && <div>Loading...</div>}
-        {order && 
+      {isLoading && <div>Loading...</div>}
+      {order && (
         <div className="container">
-            <form onSubmit={handleSubmit}>
-                <div className="form-group"> 
-                <label>Name: </label>
-                <input  type="text"
-                    required
-                    className="form-control"
-                    value={order.address.name}
-                    onChange={(e) => setName(e.target.value)}
-                    />
-                <label>Contact: </label>
-                <input  type="text"
-                    required
-                    className="form-control"
-                    value={order.address.phone}
-                    onChange={(e) => setContact(e.target.value)}
-                    />
-                <label>Email: </label>
-                <input  type="text"
-                    required
-                    className="form-control"
-                    value={order.address.email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    />
-                <label>Hall: </label>
-                <select class="form-control" id="exampleFormControlSelect1" value={order.hall} onChange={(e) => setHall(e.target.value)}>
-                    <option>Hall 4</option>
-                    <option>Hall 5</option>
-                    <option>Hall 6</option>
-                </select>
-                <input type="text" className="form-control classname" value={order.totalCost} readOnly />
-                {order.cart.map(item => {
-                    return <Item item={item} key={item.id}/>
-                })}
-                
-                </div>
-                <div className="form-group">
-                <input type="submit" value="Create User" className="btn btn-primary" />
-                </div>
-            </form>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Name: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={order.address.name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <label>Contact: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={order.address.phone}
+                onChange={(e) => setContact(e.target.value)}
+              />
+              <label>Email: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={order.address.email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label>Hall: </label>
+              <select
+                className="form-control"
+                id="exampleFormControlSelect1"
+                value={order.hall}
+                onChange={(e) => setHall(e.target.value)}
+              >
+                <option>Hall 4</option>
+                <option>Hall 5</option>
+                <option>Hall 6</option>
+              </select>
+              <br />
+              <label>Total Cost</label>
+              <input
+                type="text"
+                className="form-control classname"
+                value={order.totalCost}
+                readOnly
+              />
+              <br />
+              {order.cart.map((item, xkey) => {
+                return (
+                  <Item item={item} key={item.id} xkey={xkey} todel={todel} />
+                );
+              })}
+            </div>
+            <br />
+            <div className="form-group">
+              <input
+                type="submit"
+                value="Add Product"
+                className="btn btn-primary"
+              />
+              <input type="submit" value="Update" className="btn btn-primary" />
+            </div>
+          </form>
         </div>
-           
-        }
+      )}
     </div>
-   )
-}
+  );
+};
 
-export default Record
+export default Record;
